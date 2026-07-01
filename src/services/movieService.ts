543b5eacfa763d9079d8fb6d95667854a893ca33
Evgen Guijeen <guijeenua@gmail.com>
2026-07-02 01:27:15 +0300

    HQ v 0.3

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,8 +3,15 @@ import axios from "axios";
 
 const myKey = import.meta.env.VITE_TMDB_TOKEN;
 
-async function fetchMovies(search: string) {
-  const response = await axios.get<{ results: Movie[] }>(
+interface ResponseProps {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+async function fetchMovies(search: string): Promise<Movie[]> {
+  const response = await axios.get<ResponseProps>(
     "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1",
     {
       headers: {
